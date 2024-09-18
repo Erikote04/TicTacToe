7e02f870f09ac6dc427d2c122100214cd3755244
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-09-18 22:04:04 +0200

    Setting up

---
+++ TicTacToe/TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Erik Sebastian de Erice Jerez on 18/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
